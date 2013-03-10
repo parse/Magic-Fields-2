b5bb95199bc0c5553f4ebc5844a276413ba52828
Anders Hassis <anders.hassis@gmail.com>
2013-03-10 20:51:56 +0100

    New way of getting the MF_BASENAME so it works with symlinked directories

--- mf_constants.php
+++ mf_constants.php
@@ -3,7 +3,7 @@ global $wpdb,$blog_id;
 
 //useful for get quickly the path for  images/javascript files and css files
 //return something like: http://wordpress.local/wp-content/plugins/Magic-Fields/
-define('MF_BASENAME',plugins_url().'/'.str_replace(basename(__FILE__),"",plugin_basename(__FILE__)));
+define('MF_BASENAME',plugins_url().'/'.basename(dirname(__FILE__)).'/');
 define('MF_URL',MF_BASENAME);
 
 //return something like: /Users/user/sites/wordpres/wp-content/plugins/Magic-Fields
